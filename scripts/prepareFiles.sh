4fb16683e466fce0c5354ee1572b71bc9471a3c6
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-05-10 16:36:51 +0200

    fix: hosting the data somewhere else

--- scripts/prepareFiles.sh
+++ scripts/prepareFiles.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/bash
 
+if [ ! -e data.tgz ]; then
+    echo "Downloading test data"
+    curl https://gottliebtfreitag.de/bioinformatic/2023.05.10-test.data.tgz -o data.tgz
+fi
+if [ ! "$(md5sum data.tgz)" != "f24da3c23f433f8b4e9682c37c8993fa" ]; then
+    echo "error downloading test data"
+    exit 1
+fi
 
-cd $1
+if [ ! -d data ]; then
+    echo "Unpacking test data"
+    tar -xvzf data.tgz
+fi
 
-files=(hg38.fa hg38.fa.fai illumina.fa reads.fq sampled.sam sampled.vcf)
+files=(hg38.fa illumina.fa illumina.fa.fai reads.fq sampled.sam sampled.vcf)
 for f in ${files[@]}; do
-    echo "preparing ${f}..."
     if [ ! -e "${f}" ]; then
-        gunzip -c -k ${f}.gz > ${f}
+        echo "preparing ${f}..."
+        gunzip -c -k data/${f}.gz > data/${f}
     fi
 done
